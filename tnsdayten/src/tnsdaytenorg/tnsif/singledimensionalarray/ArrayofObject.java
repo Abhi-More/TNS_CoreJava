06ec085ad030a5113c649569f6eca40a7cbb25d4
Abhishek More <omkar@Abhishek-PC>
2023-08-10 13:37:40 +0530

    Day 10 commit 2

--- tnsdayten/src/tnsdaytenorg/tnsif/singledimensionalarray/ArrayofObject.java
+++ tnsdayten/src/tnsdaytenorg/tnsif/singledimensionalarray/ArrayofObject.java
@@ -10,7 +10,6 @@ public class ArrayofObject {
 		array[1] = new Employee(1, "Sandip Zalte", 3234567);
 		array[2] = new Employee(3, "Saish Thorat", 134567.78d);
 		
-		Arrays.sort(array, 0, 2, (a,b)->(a-b));
 		
 		for(Employee emp : array)
 		{
