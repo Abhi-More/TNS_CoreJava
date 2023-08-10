fc70d67aba2b9f4121478929c660edc644e57a17
Abhishek More <omkar@Abhishek-PC>
2023-08-10 13:36:11 +0530

    Day 10 commit 1

---
+++ tnsdayten/src/org/tnsif/wrapperclass/AutoBoxing.java
@@ -0,0 +1,15 @@
+package org.tnsif.wrapperclass;
+
+public class AutoBoxing {
+
+	public static void main(String[] args) {
+
+		// converting primitive type to object;
+		int num = 97;
+		Integer val = num;
+		
+		System.out.println(val.getClass().getName());
+
+	}
+
+}
---
+++ tnsdayten/src/org/tnsif/wrapperclass/Unboxing.java
@@ -0,0 +1,16 @@
+package org.tnsif.wrapperclass;
+
+public class Unboxing {
+
+	public static void main(String[] args) {
+
+		// unboxing
+		Character val = 'A';
+		
+		char c = val;
+		System.out.println(val.getClass());
+		System.out.println(c);
+
+	}
+
+}
