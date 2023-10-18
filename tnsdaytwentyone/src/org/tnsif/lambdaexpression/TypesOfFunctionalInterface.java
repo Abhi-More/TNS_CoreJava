b8fd130f357de808a7680712711362758b8a3107
Abhishek More <amore8196@gmail.com>
2023-10-18 11:06:22 +0530

    Labda Commit 2

--- tnsdaytwentyone/src/org/tnsif/lambdaexpression/TypesOfFunctionalInterface.java
+++ tnsdaytwentyone/src/org/tnsif/lambdaexpression/TypesOfFunctionalInterface.java
@@ -3,6 +3,7 @@ package org.tnsif.lambdaexpression;
 import java.util.function.BiFunction;
 import java.util.function.Consumer;
 import java.util.function.Function;
+import java.util.function.Predicate;
 import java.util.function.Supplier;
 
 public class TypesOfFunctionalInterface {
@@ -12,12 +13,16 @@ public class TypesOfFunctionalInterface {
 		System.out.println(sup.get());
 		
 		Consumer<Integer> cons = (n) -> {if( n > 0) 
-											System.out.println("Positive");
+											System.out.println(n + " is Positive");
 										else
-											System.out.println("positive");
+											System.out.println(n + " is Negative");
 										};
 								
 	    cons.accept(10);
+	    cons.accept(-10);
+	    
+	    Predicate<Integer> pred = (no) -> no % 2 == 0;
+	    System.out.println("Is Even : " + pred.test(33));
 										
 		Function<Integer, String> fun;
 		fun = (no) -> no > 0 ? "Positive" : "Negative";
